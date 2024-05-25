96ebccd2ce5e106a27966333f04b2c9c546508ea
Gaban03 <gabanvinicius724@gmail.com>
2024-05-25 12:22:42 -0300

    Biometria funcionando com Solenoide, LED e Buzzer

--- BiometriaOriginal/BiometriaOriginal.ino
+++ BiometriaOriginal/BiometriaOriginal.ino
@@ -2,6 +2,12 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
+#define LED_RGB_R 25
+#define LED_RGB_G 26
+#define LED_RGB_B 27
+#define BUZZER_PIN 12
+#define PIN_SOLENOIDE 32
+
 const uint32_t password = 0x0;
 HardwareSerial fingerSerial(1); // Usando a porta serial 1 do ESP32
 
@@ -9,15 +15,25 @@ Adafruit_Fingerprint fingerSensor = Adafruit_Fingerprint(&fingerSerial, password
 
 const char* ssid = "testesenai";
 const char* passwordWifi = "12345678";
-const char* pegarPosicaoBiometriaUrl = "http://10.110.12.35:8080/acesso/pegarPosicaoBiometria";
-const char* modoCadastroEndpointUrl = "http://10.110.12.35:8080/acesso/modoCadastroIniciado";
-const char* encerrarCadastroBiometriaUrl = "http://10.110.12.35:8080/acesso/encerrarCadastroBiometria";
+const char* pegarPosicaoBiometriaUrl = "http://192.168.0.13:8080/acesso/pegarPosicaoBiometria";
+const char* modoCadastroEndpointUrl = "http://192.168.0.13:8080/acesso/modoCadastroIniciado";
+const char* encerrarCadastroBiometriaUrl = "http://192.168.0.13:8080/acesso/encerrarCadastroBiometria";
+
+unsigned long lastModeCheckTime = 0;
+const unsigned long modeCheckInterval = 10000; // Intervalo de verificação do modo cadastro em milissegundos (10 segundos)
+const unsigned long ledDuration = 3000; // 3 segundos de duração do LED aceso
 
 void setup() {
     Serial.begin(9600); // Inicia a comunicação serial para mensagens de depuração
     fingerSerial.begin(57600, SERIAL_8N1, 16, 17); // RX = GPIO16, TX = GPIO17 no ESP32
     delay(100);
     
+    pinMode(LED_RGB_R, OUTPUT);
+    pinMode(LED_RGB_G, OUTPUT);
+    pinMode(LED_RGB_B, OUTPUT);
+    pinMode(BUZZER_PIN, OUTPUT);
+    pinMode(PIN_SOLENOIDE, OUTPUT);
+
     // Inicia conexão WiFi
     Serial.println("Conectando-se à rede WiFi...");
     WiFi.begin(ssid, passwordWifi);
@@ -27,10 +43,6 @@ void setup() {
     }
     Serial.println("Conectado à rede WiFi!");
 
-    // Teste de comunicação serial
-    Serial.println("Iniciando teste de comunicação serial...");
-    Serial.println("Teste de comunicação serial OK!");
-    
     // Teste de conexão com o sensor biométrico
     Serial.println("Iniciando teste de conexão com o sensor biométrico...");
     if (!fingerSensor.verifyPassword()) {
@@ -41,14 +53,9 @@ void setup() {
 }
 
 void loop() {
-    static unsigned long lastModeCheckTime = 0;
-    const unsigned long modeCheckInterval = 10000; // Intervalo de verificação do modo cadastro em milissegundos (15 segundos)
-
     // Verifica se é hora de verificar o modo cadastro
     if (millis() - lastModeCheckTime >= modeCheckInterval) {
         lastModeCheckTime = millis(); // Atualiza o último tempo de verificação
-
-        // Verifica se o modo cadastro está ativado
         verificarModoCadastro();
     }
 
@@ -70,7 +77,6 @@ void verificarModoCadastro() {
     Serial.print("Resposta do servidor (Modo Cadastro): ");
     Serial.println(response);
 
-    // Verificar se o modo de cadastro está iniciado
     if (httpResponseCode == 200 && response == "true") {
         Serial.println("Modo de cadastro iniciado. Armazenando digital...");
         storeFingerprint(); // Se o modo de cadastro estiver iniciado, armazena a digital
@@ -82,13 +88,9 @@ void verificarModoCadastro() {
 }
 
 void storeFingerprint() {
-    // Obtém a posição para armazenar a digital do endpoint
     int location = obterPosicaoBiometria();
 
-    // Verifica se a posição é válida ou não
-    if(location < 1 || location > 149)
-    {
-        // Se chegou aqui a posição obtida é inválida, então abortamos os próximos passos
+    if(location < 1 || location > 149) {
         Serial.println(F("Posição inválida"));
         return;
     }
@@ -98,58 +100,38 @@ void storeFingerprint() {
 
     Serial.println(F("Encoste o dedo no sensor"));
 
-    //Espera até pegar uma imagem válida da digital
     while (fingerSensor.getImage() != FINGERPRINT_OK);
     
-    //Converte a imagem para o primeiro padrão
-    if (fingerSensor.image2Tz(1) != FINGERPRINT_OK)
-    {
-        //Se chegou aqui deu erro, então abortamos os próximos passos
+    if (fingerSensor.image2Tz(1) != FINGERPRINT_OK) {
         Serial.println(F("Erro image2Tz 1"));
         return;
     }
     
     Serial.println(F("Tire o dedo do sensor"));
-
     delay(2000);
 
-    //Espera até tirar o dedo
     while (fingerSensor.getImage() != FINGERPRINT_NOFINGER);
 
-    //Antes de guardar precisamos de outra imagem da mesma digital
     Serial.println(F("Encoste o mesmo dedo no sensor"));
 
-    //Espera até pegar uma imagem válida da digital
     while (fingerSensor.getImage() != FINGERPRINT_OK);
 
-    //Converte a imagem para o segundo padrão
-    if (fingerSensor.image2Tz(2) != FINGERPRINT_OK)
-    {
-        //Se chegou aqui deu erro, então abortamos os próximos passos
+    if (fingerSensor.image2Tz(2) != FINGERPRINT_OK) {
         Serial.println(F("Erro image2Tz 2"));
         return;
     }
 
-    //Cria um modelo da digital a partir dos dois padrões
-    if (fingerSensor.createModel() != FINGERPRINT_OK)
-    {
-        //Se chegou aqui deu erro, então abortamos os próximos passos
+    if (fingerSensor.createModel() != FINGERPRINT_OK) {
         Serial.println(F("Erro createModel"));
         return;
     }
 
-    //Guarda o modelo da digital no sensor
-    if (fingerSensor.storeModel(location) != FINGERPRINT_OK)
-    {
-        //Se chegou aqui deu erro, então abortamos os próximos passos
+    if (fingerSensor.storeModel(location) != FINGERPRINT_OK) {
         Serial.println(F("Erro storeModel"));
         return;
     }
 
-    //Se chegou aqui significa que todos os passos foram bem sucedidos
     Serial.println(F("Sucesso!!!"));
-
-    // Após o cadastro ser concluído com sucesso, chamamos a função para encerrar o cadastro
     encerrarCadastroBiometria();
 }
 
@@ -158,7 +140,6 @@ void encerrarCadastroBiometria() {
     http.begin(encerrarCadastroBiometriaUrl);
     http.addHeader("Content-Type", "application/json");
 
-    // Enviamos o valor false no corpo da requisição para indicar que o cadastro deve ser encerrado
     int httpResponseCode = http.POST("false");
     if (httpResponseCode == 200) {
         Serial.println("Cadastro de biometria encerrado com sucesso!");
@@ -181,7 +162,6 @@ int obterPosicaoBiometria() {
     int position = -1; // Valor padrão caso algo dê errado
 
     if (httpResponseCode == 200) {
-        // Se a solicitação for bem-sucedida, leia a posição da resposta
         String response = http.getString(); 
         position = response.toInt(); // Converta a resposta para inteiro
     } else {
@@ -192,28 +172,69 @@ int obterPosicaoBiometria() {
     return position;
 }
 
-// Função para verificar a presença de uma impressão digital
 void checkFingerprint() {
-    // Verifica se uma imagem de digital foi detectada
     if (fingerSensor.getImage() == FINGERPRINT_OK) {
         Serial.println("Digital detectada! Verificando...");
         
-        // Converte a imagem em um formato adequado para comparação
         if (fingerSensor.image2Tz() != FINGERPRINT_OK) {
             Serial.println("Erro ao converter a imagem da digital.");
             return;
         }
         
-        // Procura por esta digital no banco de dados
         if (fingerSensor.fingerFastSearch() != FINGERPRINT_OK) {
             Serial.println("Digital não encontrada no banco de dados.");
+            turnOnLED("vermelho");
+            playLongSound();
+            delay(ledDuration);
+            turnOffLED();
             return;
         }
-        
-        // Se chegou até aqui, a digital foi encontrada
+
         Serial.print("Digital encontrada com confiança de ");
         Serial.print(fingerSensor.confidence);
         Serial.print(" na posição ");
         Serial.println(fingerSensor.fingerID);
+        
+        turnOnLED("verde");
+        playShortSound();
+        activateSolenoide();
+        delay(ledDuration);
+        turnOffLED();
     }
+}
+
+void turnOnLED(const char* color) {
+    digitalWrite(LED_RGB_R, LOW);
+    digitalWrite(LED_RGB_G, LOW);
+    digitalWrite(LED_RGB_B, LOW);
+
+    if (strcmp(color, "verde") == 0) {
+        digitalWrite(LED_RGB_G, HIGH);
+    } else if (strcmp(color, "vermelho") == 0) {
+        digitalWrite(LED_RGB_R, HIGH);
+    }
+}
+
+void turnOffLED() {
+    digitalWrite(LED_RGB_R, LOW);
+    digitalWrite(LED_RGB_G, LOW);
+    digitalWrite(LED_RGB_B, LOW);
+}
+
+void activateSolenoide() {
+    digitalWrite(PIN_SOLENOIDE, HIGH);
+    delay(5000); // Ativa por 5 segundos
+    digitalWrite(PIN_SOLENOIDE, LOW);
+}
+
+void playShortSound() {
+    tone(BUZZER_PIN, 1000); // 1000 Hz
+    delay(100); 
+    noTone(BUZZER_PIN);
+}
+
+void playLongSound() {
+    tone(BUZZER_PIN, 1000); // 1000 Hz
+    delay(2000); // 2 segundos
+    noTone(BUZZER_PIN);
 }
